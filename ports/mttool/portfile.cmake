df22ce0b3b1719be77e984932a1e1659ec2b2c84
hxf@xiao_yixiao <hxfengv@163.com>
2026-05-09 22:30:40 +0800

    [mttool] add new port 1.0.3

--- ports/mttool/portfile.cmake
+++ ports/mttool/portfile.cmake
@@ -1,6 +1,3 @@
-include(vcpkg-cmake)
-include(vcpkg-cmake-config)
-
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL https://github.com/feng9201/mttool
@@ -13,13 +10,22 @@ if("eventpp" IN_LIST FEATURES)
     set(ENABLE_EVENTPP ON)
 endif()
 
-vcpkg_cmake_configure(
-    SOURCE_PATH "${SOURCE_PATH}"
-    OPTIONS
+# 手动运行 cmake configure + install（vcpkg 内置函数，无需额外 include）
+vcpkg_execute_required_process(
+    COMMAND "${CMAKE_COMMAND}"
+        -S "${SOURCE_PATH}"
+        -B "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel"
         -DMT_TOOL_ENABLE_EVENTPP=${ENABLE_EVENTPP}
+        -DCMAKE_INSTALL_PREFIX="${CURRENT_PACKAGES_DIR}"
+    WORKING_DIRECTORY "${SOURCE_PATH}"
+    LOGNAME configure
 )
 
-vcpkg_cmake_install()
+vcpkg_execute_required_process(
+    COMMAND "${CMAKE_COMMAND}" --install "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel"
+    WORKING_DIRECTORY "${SOURCE_PATH}"
+    LOGNAME install
+)
 
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/LICENSE"
     DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
